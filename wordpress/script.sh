c6fbecf1b24d9f09897dc7c7b98c5061bd0f474f
afonsomlcarvalho <afonsomlc28@gmail.com>
2023-07-27 14:27:17 +0100

    nginx fully operational, problems with wordpress

--- wordpress/script.sh
+++ wordpress/script.sh
@@ -8,4 +8,7 @@ rm -rf wordpress.tar.gz/* wordpress/*
 
 cp wp-config-sample.php wp-config.php
 
-sed -i "s/database_name_here/wordpress/" wp-config.php
+sed -i "s/database_name_here/wordpress/g" wp-config.php
+sed -i "s/username_here/amorais-/g" wp-config.php
+sed -i "s/password_here/malandro/g" wp-config.php
+sed -i "s/localhost/amorais-.42.fr/g" wp-config.php
