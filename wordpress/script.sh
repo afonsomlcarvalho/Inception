9eeb47163f6104c490fbecf8bc359c1284bd061d
afonsomlcarvalho <afonsomlc28@gmail.com>
2023-08-11 11:09:22 +0100

    nao sei

--- wordpress/script.sh
+++ wordpress/script.sh
@@ -10,7 +10,7 @@ rm -rf wordpress.tar.gz/* wordpress/
 
 cp wp-config-sample.php wp-config.php
 
-sed -i "s/database_name_here/wordpress/g" wp-config.php
-sed -i "s/username_here/amorais-/g" wp-config.php
-sed -i "s/password_here/malandro/g" wp-config.php
-sed -i "s/localhost/amorais-.42.fr/g" wp-config.php
+sed -i "s/database_name_here/$DATABASENAME/g" wp-config.php
+sed -i "s/username_here/$USERNAME/g" wp-config.php
+sed -i "s/password_here/$PASSWORD/g" wp-config.php
+sed -i "s/localhost/$LOCALHOST/g" wp-config.php
