4fb9080bd1b75d4d40d142ea9f2f3348c9b92b66
amorais- <amorais-@student.42lisboa.com>
2023-09-13 11:43:11 +0100

    done

--- srcs/requirements/mariadb/tools/mariadb_script.sh
+++ srcs/requirements/mariadb/tools/mariadb_script.sh
@@ -2,6 +2,12 @@
 
 service mysql start;
 
+if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]
+then
+	echo -e "\033[1;33mMariaDB already setup\033[0m"
+
+else
+
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
 mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
@@ -12,8 +18,12 @@ mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
 mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
-
 mysql -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
 
+fi
+
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
+
 exec mysqld_safe
+
+exec "$@"
